7679df9bc3a1bbe3e0496c6cb7a45ee2da2e7c07
doddyph <d0dd33ph@gmail.com>
2014-12-19 23:23:03 +0700

    [update] add function to cached inverse matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,28 @@
 makeCacheMatrix <- function(x = matrix()) {
+    invmatrix <- NULL
     
+    set <- function(y) {
+        x <<- y
+        invmatrix <<- NULL
+    }
+    
+    get <- function() x
+    setInv <- function(xInvmatrix) invmatrix <<- xInvmatrix
+    getInv <- function() invmatrix
+    
+    list(set = set, get = get, setInv = setInv, getInv = getInv)
 }
 
 cacheSolve <- function(x, ...) {
+    invmatrix <- x$getInv()
     
+    if (!is.null(invmatrix)) {
+        message("getting cached data")
+        return(invmatrix)
+    }
+    
+    data <- x$get()
+    invmatrix <- solve(data, ...)
+    x$setInv(invmatrix)
+    invmatrix
 }
